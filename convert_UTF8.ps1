1ffec1433b0fb26369529ffd795d0114b5719581
dredknight <gangstarrr@abv.bg>
2025-04-12 17:10:07 +0300

    wheel changes

--- convert_UTF8.ps1
+++ convert_UTF8.ps1
@@ -4,16 +4,16 @@ Write-Output "Processing directory..."
 # - UTF-8 is used by this repository so github can show diff during PRs
 $encoding = [System.Text.Encoding]::UTF8
 
-Get-ChildItem -Path $folderPath -Recurse -Filter "*.bak" | Remove-Item -Force
+Get-ChildItem -Path $directory -Recurse -Filter "*.bak" | Remove-Item -Force
 
 Get-ChildItem -Path $directory -Filter "*.txt" -Recurse | ForEach-Object {
-	$inputFile = $_.FullName
-	Write-Output "Converting: $inputFile"
+    $inputFile = $_.FullName
+    Write-Output "Converting: $inputFile"
 
-	if ($_.IsReadOnly) { $_.IsReadOnly = $false }
-	
-	## Read the file and write it back in UTF-8
-	$content = Get-Content -Raw -Path $inputFile
-	[System.IO.File]::WriteAllText($inputFile, $content, $encoding)
+    if ($_.IsReadOnly) { $_.IsReadOnly = $false }
+
+    ## Read the file and write it back in UTF-8
+    $content = Get-Content -Raw -Path $inputFile
+    [System.IO.File]::WriteAllText($inputFile, $content, $encoding)
 }
 Write-Output "Conversion complete!"
